dae6fa8f333ef9d6ad8710212e728f5fde70353f
mayaki <mayakijnr@gmail.com>
2024-08-19 09:04:55 +0100

    Changes bubble algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,23 +12,24 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j, swapped;
 	int temp;
 
+	if (!array || size < 2)
+		return;
+
 	for (i = 0; i < size; i++)
 	{
 
 		swapped = 0;
-		for (j = 1; j < (size - i); j++)
+		for (j = 0; j < (size - i - 1); j++)
 		{
-			if (array[j - 1] > array[j])
+			if (array[j] > array[j + 1])
 			{
-				temp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = temp;
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
 			}
-
-			swapped = 1;
 			print_array(array, size);
 		}
-
 		if (!swapped)
 			break;
 	}
