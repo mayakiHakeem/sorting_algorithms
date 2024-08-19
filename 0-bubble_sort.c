6d5a7d509c079998394ee62f84efb59bdc84d2ab
mayaki <mayakijnr@gmail.com>
2024-08-19 09:36:54 +0100

    Fix betty errors

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,48 @@
+#include <stdio.h>
 #include "sort.h"
 
+
 /**
- * bubble_sort - sort using bubble sort algo
- * @array: data to be sorted
- * @size: size of the array
- *
- * Return: Nothing
+ * bubble_sort - Sort integer array in ascending order using Bubble sort
+ * @array: The array to be sorted
+ * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j, swapped;
-	int temp;
+	size_t i, j;
+	int swapped;
 
 	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-
 		swapped = 0;
-		for (j = 0; j < (size - i - 1); j++)
+		for (j = 0; j < size - 1 - i; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				swap(&array[j], &array[j + 1]);
+				print_array(array, size);
 				swapped = 1;
 			}
-			print_array(array, size);
 		}
+
 		if (!swapped)
 			break;
 	}
+}
+
+
+
+/**
+ * swap - Swaps two elements in an array
+ * @a: Pointer to the first element
+ * @b: Pointer to the second element
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
