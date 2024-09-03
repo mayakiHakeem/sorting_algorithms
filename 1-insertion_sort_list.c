dda65c72c717ff44e6920bfa3077233adbcfa671
mayaki <mayakijnr@gmail.com>
2024-09-04 00:02:17 +0100

    Add insertion sort for linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorting a doubly linked list with insertion sort
+ * @list: doubly linked list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current, *temp;
+
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	/*move key to second node*/
+	current = (*list)->next;
+	while (current)
+	{
+		temp = current;
+		while(temp->prev && temp->prev->n > temp->n)
+		{
+			swap_nodes(list, temp->prev, temp);
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
+
+/**
+ * swap_nodes - swap 2 nodes of @list
+ * @list: list to swap nodes of
+ * @a: node to swap
+ * @b: node to swap
+ */
+void swap_nodes(listint_t **list, listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	else
+		*list = b;
+
+	if (b->next)
+		b->next->prev = a;
+
+	a->next = b->next;
+	b->next = a;
+	b->prev = a->prev;
+	a->prev = b;
+}
--- sort.h
+++ sort.h
@@ -21,5 +21,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void swap(int *a, int *b);
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *a, listint_t *b);
 
 #endif
